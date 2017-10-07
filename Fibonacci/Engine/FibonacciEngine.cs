806e2b3b1f8396a33e31f19a1a6cd7fd2f6fad18
Peter <prlaning85@gmail.com>
2017-10-07 18:01:33 +1000

    Add project files.

---
+++ Fibonacci/Engine/FibonacciEngine.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci.Engine
+{
+    public class FibonacciEngine
+    {
+        public int MaxIterations { get; set; }
+
+        public List<ulong> CalculateSequence(int max)
+        {
+            ulong previous = 0;
+            ulong current = 1;
+
+            var sequence = new List<ulong>() { previous };
+
+            for (var i = 0; i < (max - 1); i++)
+            {
+                current = current + previous;
+                previous = current - previous;
+
+                sequence.Add(current);
+            }
+
+            return sequence;
+        }
+
+        public void Intro()
+        {
+            Console.WriteLine(" Fibonacci Sequencer ");
+            Console.WriteLine("=====================");
+            Console.WriteLine("");
+            Console.Write("Input max number of iterations: ");
+
+            MaxIterations = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void OutputSequence(List<ulong> seq)
+        {
+            int index = 1;
+
+            foreach (ulong num in seq)
+                Console.WriteLine(String.Format("{0}: {1}", index++, num.ToString("#,##0")));
+        }
+    }
+}
---
+++ Fibonacci/Program.cs
@@ -0,0 +1,25 @@
+﻿using Fibonacci.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fibonacci = new FibonacciEngine();
+
+            fibonacci.Intro();
+
+            var sequence = fibonacci.CalculateSequence(fibonacci.MaxIterations);
+
+            fibonacci.OutputSequence(sequence);
+
+            Console.ReadKey();
+        }
+    }
+}
